85b0494e4036cdb474eeec6258be5e9ecdadca01
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 21:36:16 +0300

    Scripted auto-commit on change (2020-02-11 21:36:16) by gitwatch.sh

--- src/main/java/actorSystem.java
+++ src/main/java/actorSystem.java
@@ -4,24 +4,18 @@ import akka.japi.pf.ReceiveBuilder;
 
 
 import java.util.HashMap;
+import java.util.List;
 
 import static akka.actor.TypedActor.self;
 
 public class CacheActor extends AbstractActor {
 
-        private HashMap  <String, Long> store = new HashMap<>();
-        //id res
-    //создаем в actorSystem — актор который принимает две команды — поиск
-        //уже готового результата тестирования и результат тестрования.
+    private List<String> serversList;
         @Override
-        public AbstractActor.Receive createReceive() {
+        public Receive createReceive() {
             return ReceiveBuilder.create()
-                    .match(SearchResult.class, m -> {
-                        String url = m.getURL();
-                        Long count = (long) m.getCount();
-                        if (store.containsKey(m.getURL())) {
-                            sender().tell(new TestResult(m.getURL(), count), self());
-                        }
+                    .match(Servers.class, m -> {
+                        
                         else sender().tell(new TestResult(m.getURL(), -1l), self());
 
                     })
