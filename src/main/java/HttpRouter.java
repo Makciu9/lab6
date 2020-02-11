ac193e67040e387bbbf9e5df267bf90167444e22
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 21:49:56 +0300

    Scripted auto-commit on change (2020-02-11 21:49:56) by gitwatch.sh

--- src/main/java/HttpRouter.java
+++ src/main/java/HttpRouter.java
@@ -4,6 +4,7 @@ import akka.http.javadsl.Http;
 import akka.http.javadsl.server.Route;
 import akka.http.javadsl.model.*;
 import akka.japi.Pair;
+import akka.pattern.Patterns;
 
 import static akka.http.javadsl.server.Directives.route;
 
