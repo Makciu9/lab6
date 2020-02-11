8ff4efcbbe136979535a6b296c5d748aa2a13d3e
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 23:12:17 +0300

    Scripted auto-commit on change (2020-02-11 23:12:17) by gitwatch.sh

--- src/main/java/ZookeeperExecutor.java
+++ src/main/java/ZookeeperExecutor.java
@@ -2,11 +2,14 @@ import akka.actor.ActorRef;
 import org.apache.zookeeper.ZooKeeper;
 
 public class ZookeeperExecutor {
+    private static final String SERVER = "localhost";
+    private static final int PORT = 8080;
     private ZooKeeper zoo;
     private ActorRef cacheActor;
 
     public ZookeeperExecutor(ActorRef cacheActor, int serverPort){
         this.cacheActor = cacheActor;
-        zoo = new ZooKeeper(
+        zoo = new ZooKeeper(SERVER + "|" + PORT,
+                
     }
 }
