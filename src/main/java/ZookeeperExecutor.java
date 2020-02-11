7650d26df10a33f861b6f6c6a69f681cf2e29f86
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 23:09:56 +0300

    Scripted auto-commit on change (2020-02-11 23:09:56) by gitwatch.sh

--- src/main/java/ZookeeperExecutor.java
+++ src/main/java/ZookeeperExecutor.java
@@ -4,4 +4,8 @@ import org.apache.zookeeper.ZooKeeper;
 public class ZookeeperExecutor {
     private ZooKeeper zoo;
     private ActorRef cacheActor;
+
+    public ZookeeperExecutor(ActorRef cacheActor, int serverPort){
+        
+    }
 }
