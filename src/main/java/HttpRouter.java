15167eeed749f884bd98650cfa966722a6fc16d7
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 22:54:10 +0300

    Scripted auto-commit on change (2020-02-11 22:54:10) by gitwatch.sh

--- src/main/java/HttpRouter.java
+++ src/main/java/HttpRouter.java
@@ -42,7 +42,7 @@ class HttpRouter {
 
     private CompletionStage<HttpResponse> redirect(Http http, String url, int count) {
         return Patterns.ask(cacheActor, new CacheActor, Duration.ofMillis(5000)
-                
+                .thenCompose(randServer -> {
     }
 
 
