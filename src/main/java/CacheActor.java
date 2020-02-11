e55b7803df891966a60685776fabcd2834e33ab7
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 21:41:27 +0300

    Scripted auto-commit on change (2020-02-11 21:41:27) by gitwatch.sh

--- src/main/java/CacheActor.java
+++ src/main/java/CacheActor.java
@@ -22,6 +22,7 @@ public class CacheActor extends AbstractActor {
                     .match(CacheActor.class, m -> {
                         int randServerIdx = new Random().nextInt(serversList.size());
                         String randServer = serversList.get(randServerIdx);
+                        System.out.println(
 
                     })
             .build();
